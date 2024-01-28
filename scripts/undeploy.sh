8097925fe7eb0edfad5944e53261cf33382f9e50
Will Cygan <wcygan.io@gmail.com>
2024-01-27 22:51:44 -0600

    create deploy and undeploy scripts

---
+++ scripts/undeploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the name of the current directory
+current_directory=$(basename "$(pwd)")
+
+# Check if the current directory is not "kafka-on-kubernetes"
+if [ "$current_directory" != "kafka-on-kubernetes" ]; then
+    echo "Please run this from the root directory of the project (kafka-on-kubernetes)."
+    exit 1
+fi
