31d3e15e8e5604453f9760b8a370a88bb3610815
Will Cygan <wcygan.io@gmail.com>
2024-02-06 23:13:08 -0600

    try fixing docker image build + push

--- producer/go.mod
+++ producer/go.mod
@@ -1,16 +1,15 @@
 module github.com/wcygan/kafka-on-kubernetes/producer
 
 go 1.21.6
-//TODO: Something about this go module file is causing github actions to fail because
-//      the generated go files aren't being found.
+
 require (
 	github.com/segmentio/kafka-go v0.4.47
 	github.com/wcygan/kafka-on-kubernetes/generated/go v0.0.0
 )
 
 require (
-	github.com/klauspost/compress v1.15.9 // indirect
-	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/klauspost/compress v1.17.5 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )
 
--- producer/main.go
+++ producer/main.go
@@ -3,48 +3,31 @@ package main
 import (
 	"context"
 	"github.com/segmentio/kafka-go"
-	"github.com/wcygan/kafka-on-kubernetes/generated/go/packet/v1"
+	packetv1 "github.com/wcygan/kafka-on-kubernetes/generated/go/packet/v1"
 	"log"
-	"time"
+	"math/rand"
 )
 
 func main() {
-	write()
-}
-
-func write() {
 	w := &kafka.Writer{
 		Addr:     kafka.TCP("example-cluster-kafka-bootstrap:9092"),
 		Topic:    "packet",
 		Balancer: &kafka.LeastBytes{},
 	}
 
-	// produce a packet every 2 seconds
-	ticker := time.NewTicker(2 * time.Second)
-	defer ticker.Stop()
-
-	// initialize packet number
-	packetNumber := int64(1)
-
 	for {
-		select {
-		case <-ticker.C:
-			packet := packetv1.Packet{Number: packetNumber}
-			log.Println("Producing packet: ", packet.Number)
+		packet := packetv1.Packet{Number: rand.Int63()}
+		log.Println("Producing packet: ", packet.Number)
 
-			err := w.WriteMessages(context.Background(),
-				kafka.Message{
-					Key:   []byte("Key"),
-					Value: []byte(packet.String()),
-				},
-			)
+		err := w.WriteMessages(context.Background(),
+			kafka.Message{
+				Key:   []byte("Key"),
+				Value: []byte(packet.String()),
+			},
+		)
 
-			if err != nil {
-				log.Fatal("failed to write messages:", err)
-			}
-
-			// increment packet number
-			packetNumber++
+		if err != nil {
+			log.Fatal("failed to write messages:", err)
 		}
 	}
 }
